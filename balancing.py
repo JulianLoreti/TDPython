27a049b9f6c803a08f7e8f3d0b0db6285228bea2
joshwprice <joshwprice@gmail.com>
2016-04-20 18:16:30 -0400

    Added files via upload

--- balancing.py
+++ balancing.py
@@ -1,33 +1,36 @@
 """ Global Defaults and configuration values for game balancing and alike. """
 
 # Player Defaults
-DEF_NAME = "Player1"
-START_GOLD = 1000
-START_LIVES = 100
-ROUND = 0
+DEF_NAME = "Player1"    # If one is not provided
+START_GOLD = 1000       # Player's initial gold amount (where do they earn more?)
+START_LIVES = 100       # Player's initial number of Lives   
+ROUNDS = 4              # Number of rounds before victory
+ROUND_MULT = 1.2         # Enemy number multiplier per round
 
 # Tower Attributes
-T1DAM = 20  # Tower 1 Damage
+T1_DAM = 20  # Tower 1 Damage
 T1_RAN = 2   # Tower 1 Range  
 T1_SPD = 5   # Tower 1 Speed
 
-T1_DAM = 15  # Tower 1 Damage
-T1_RAN = 5   # Tower 1 Range  
-T1_SPD = 3   # Tower 1 Speed
+T1_DAM = 15  # Tower 2 Damage
+T1_RAN = 5   # Tower 2 Range  
+T1_SPD = 3   # Tower 2 Speed
 
-T1_DAM = 30  # Tower 1 Damage
-T1_RAN = 3   # Tower 1 Range  
-T1_SPD = 4   # Tower 1 Speed
+T1_DAM = 30  # Tower 3 Damage
+T1_RAN = 3   # Tower 3 Range  
+T1_SPD = 4   # Tower 3 Speed
 
 # Enemy Attributes
-E1_HP = 20
-E1_SPD = 1
+START_LOC = [0,14] # (y, x)
 
-E2_HP = 20
-E2_SPD = 2
+E1_HP = 20  # Enemy 1 Health
+E1_SPD = 1  # Enemy 1 Speed
 
-E3_HP = 20
-E3_SPD = 3
+E2_HP = 20  # Enemy 2 Health
+E2_SPD = 2  # Enemy 2 Speed
+
+E3_HP = 20  # Enemy 3 Health
+E3_SPD = 3  # Enemy 3 Speed
 
 # Images
 T1_PIC = "./images/tower1.png"
---
+++ jclasses.py
@@ -0,0 +1,180 @@
+import sys, time
+from random import random
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *	
+
+###########################################################
+
+class Tower(QLabel):
+	def __init__(self, y_loc, x_loc, pic, dam, ran, spd):
+		super(Sprite, self).__init__(self)
+		
+		self.position = [y_loc, x_loc]
+		self.pic = pic
+		self.damage = dam
+		self.range = ran
+		self.speed = spd
+		self.clickable = True
+		self.flag = True
+
+		self.setPixmap(QPixmap(pic))
+		self.setSizePolicy (QSizePolicy.Preferred, QSizePolicy.Preferred)
+		self.setMaximumWidth(48)
+		self.setMaximumHeight(48)
+		self.resize(48,48)
+		
+	def mouseMoveEvent(self, e):
+		if e.buttons() != Qt.LeftButton:
+			return
+		if self.clickable and self.flag:
+			# write the relative cursor position to mime data
+			mimeData = QMimeData()
+			# simple string with 'x,y'
+			mimeData.setText('%d,%d,%s' % (e.x(), e.y(), self.pic))
+
+			# let's make it fancy. we'll show a "ghost" of the button as we drag
+			# grab the button to a pixmap
+			pixmap = QPixmap.grabWidget(self)
+
+			# below makes the pixmap half transparent
+			painter = QPainter(pixmap)
+			painter.setCompositionMode(painter.CompositionMode_DestinationIn)
+			painter.fillRect(pixmap.rect(), QColor(0, 0, 0, 127))
+			painter.end()
+
+			# make a QDrag
+			drag = QDrag(self)
+			# put our MimeData
+			drag.setMimeData(mimeData)
+			# set its Pixmap
+			drag.setPixmap(pixmap)
+			# shift the Pixmap so that it coincides with the cursor position
+			drag.setHotSpot(e.pos())
+
+			# start the drag operation
+			# exec_ will return the accepted action from dropEvent
+			if drag.exec_(Qt.CopyAction | Qt.MoveAction) == Qt.MoveAction:
+				print 'moved'
+			else:
+				print 'copied'
+
+	def mousePressEvent(self, e):
+		if (self.flag == False):
+			if e.button() == Qt.LeftButton:
+				print "pressed"
+
+class Tower1(Tower):
+	def __init__(self, y_loc, x_loc):
+		super(Tower1, self)__init__(y_loc, x_loc, T1_PIC, T1_DAM, T1_RAN, T1_SPD)
+
+class Tower2(Tower):
+	def __init__(self, y_loc, x_loc):
+		super(Tower2, self)__init__(y_loc, x_loc, T2_PIC, T2_DAM, T2_RAN, T2_SPD)
+
+class Tower3(Tower):
+	def __init__(self, y_loc, x_loc):
+		super(Tower3, self)__init__(y_loc, x_loc, T3_PIC, T3_DAM, T3_RAN, T3_SPD)
+
+###########################################################
+
+class Enemy(QLabel):
+	def __init__(self, y_loc, x_loc, pic, hp, spd):
+		super(Enemy, self).__init__(self)
+		
+		self.location = [y_loc, x_loc]
+		self.health = hp
+		self.pic = pic
+
+		self.setPixmap(QPixmap(pic))
+		self.setSizePolicy (QSizePolicy.Preferred, QSizePolicy.Preferred)
+		self.setMaximumWidth(48)
+		self.setMaximumHeight(48)
+		self.resize(48,48)
+
+	def enemy_start(self, gameBoard):
+	
+		locationStart = gameBoard[0][14]
+		i = 0
+		j = 14
+		plusCount = 0
+		#57 spaces
+		count = 1
+		location = gameBoard[i][j]
+		while(count <= 56):
+			if(location == "D"):
+				i = i + 1
+
+			#time.sleep(0.2)
+			if(location == "L"):
+				j = j - 1
+
+			#time.sleep(0.2)
+
+			if(location == "R"):
+				j = j + 1
+
+			#time.sleep(0.2)
+			if(location == "U"):
+				i = i - 1
+
+			#time.sleep(0.2)
+			if(plusCount == 0 and location == "T"):
+				plusCount = 1
+				i = i - 1
+				location = "U"
+
+			#time.sleep(0.2)
+			if(plusCount == 1 and location == "T"):
+				j = j + 1
+				print "2nd T location", location
+				location = "R"
+				#time.sleep(0.2)
+				if(location == "E"):
+					print "life lost"
+					break
+
+			count = count + 1
+			location = gameBoard[i][j]
+		
+class Enemy1(Enemy):
+	def __init__(self, y_loc, x_loc):
+		super(Enemy1, self)__init__(y_loc, x_loc, E1_PIC, E1_HP, E1_SPD)
+
+class Enemy2(Enemy):
+	def __init__(self, y_loc, x_loc):
+		super(Enemy2, self)__init__(y_loc, x_loc, E2_PIC, E2_HP, E2_SPD)
+
+class Enemy3(Enemy):
+	def __init__(self, y_loc, x_loc):
+		super(Enemy3, self)__init__(y_loc, x_loc, E3_PIC, E3_RAN, E3_SPD)
+
+#############################################################
+
+class Player:
+	def __init__(self,name=DEF_NAME):
+		self.name = name
+		self.gold = START_GOLD
+		self.lives = START_LIVES
+		self.round = 0
+
+#############################################################
+
+class Wave:
+	def __init__(self, player, roundNum, enemiesList, board):
+		self.round = roundNum
+		self.enemies = 0
+
+	def send_enemy(self, player):
+		if(self.round == 1):
+			self.enemies = 25
+		else:
+			self.enemies = int(float(self.enemies * ROUND_MULT))
+
+		for i in range(self.enemies):
+			num = random(3)
+			if num == 0:
+				e = Enemy1(START_LOC[0], START_LOC[1])
+			e = Enemy()
+			e.enemy_start(board)
+
+#############################################################
