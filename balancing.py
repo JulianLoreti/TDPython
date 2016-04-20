8c406879ce80449c1e1784f12ccbf902b2ff8ae0
joshwprice <joshwprice@gmail.com>
2016-04-20 15:42:16 -0400

    balancing.py
    New File for global values  all in one place

---
+++ balancing.py
@@ -0,0 +1,48 @@
+""" Global Defaults and configuration values for game balancing and alike. """
+
+# Player Defaults
+DEF_NAME = "Player1"
+START_GOLD = 1000
+START_LIVES = 100
+ROUND = 0
+
+# Tower Attributes
+T1DAM = 20  # Tower 1 Damage
+T1_RAN = 2   # Tower 1 Range  
+T1_SPD = 5   # Tower 1 Speed
+
+T1_DAM = 15  # Tower 1 Damage
+T1_RAN = 5   # Tower 1 Range  
+T1_SPD = 3   # Tower 1 Speed
+
+T1_DAM = 30  # Tower 1 Damage
+T1_RAN = 3   # Tower 1 Range  
+T1_SPD = 4   # Tower 1 Speed
+
+# Enemy Attributes
+E1_HP = 20
+E1_SPD = 1
+
+E2_HP = 20
+E2_SPD = 2
+
+E3_HP = 20
+E3_SPD = 3
+
+# Images
+T1_PIC = "./images/tower1.png"
+T2_PIC = "./images/tower2.png"
+T3_PIC = "./images/tower3.png"
+
+E1_PIC = "./images/enemy1.png"
+E2_PIC = "./images/enemy2.png"
+E3_PIC = "./images/enemy3.png"
+
+BG_PIC = "./images/newbg.png"
+TB_PIC = "./images/toolbar.png"
+ST_PIC = "./images/start.png"
+NX_PIC = "./images/next.png"
+
+HRT_PIC = "./images/heart.png"
+GLD_PIC = "./images/gold.png"
+BUL_PIC = "./images/bullet.png"
